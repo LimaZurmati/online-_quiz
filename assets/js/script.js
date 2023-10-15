1cdccbbf0143d5fe6d3d3b82c1f6bb780831531f
LimaZurmati <urzalalima@gmail.com>
2023-10-15 17:30:08 +0000

    add first var questions with the answer option

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const questions = [
+    {
+        question: 'what is the second name of kabul?',
+        answer: [
+            {text:"kabul",correct:true},
+            {text:"herat",correct:false},
+            {text:"ghazni",correct:false},
+            {text:"ghor",correct:false},
+
+        ]
+    }
+]
