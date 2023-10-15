0113f52cc02dfb56680d8f20291d39230a952f26
LimaZurmati <urzalalima@gmail.com>
2023-10-15 17:55:34 +0000

    add 3 var for question,answer and next button

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,32 +10,36 @@ const questions = [
         ]
     },
     {
-        question: 'what is the second name of kabul?',
+        question: 'what is css?',
         answer: [
-            {text:"kabul",correct:true},
-            {text:"herat",correct:false},
-            {text:"ghazni",correct:false},
-            {text:"ghor",correct:false},
+            {text:"stands for Cascading Style Sheets",correct:true},
+            {text:"page",correct:false},
+            {text:"style ",correct:false},
+            {text:"design",correct:false},
 
         ]
     },
     {
-        question: 'what is the second name of kabul?',
+        question: 'what is html?',
         answer: [
-            {text:"kabul",correct:true},
-            {text:"herat",correct:false},
-            {text:"ghazni",correct:false},
-            {text:"ghor",correct:false},
+            {text:"Hyper Text Markup Language",correct:true},
+            {text:"programming languge",correct:false},
+            {text:"coding",correct:false},
+            {text:"none",correct:false},
 
         ]
     },{
-        question: 'what is the second name of kabul?',
+        question: 'what is js?',
         answer: [
-            {text:"kabul",correct:true},
-            {text:"herat",correct:false},
-            {text:"ghazni",correct:false},
-            {text:"ghor",correct:false},
+            {text:" scripting language",correct:true},
+            {text:"scripting",correct:false},
+            {text:"web",correct:false},
+            {text:"none",correct:false},
 
         ]
     }
-]
+];
+
+ const questionPart = document.getElementById("question");
+ const answerButton = document.getElementById("answer");
+ const nextButton = document.getElementById("next");
