505095797e18cb84da1b86ccb371bca27a896400
LimaZurmati <urzalalima@gmail.com>
2023-10-17 19:13:00 +0000

    add reset  function to hide pervious answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,129 @@
-const questions = [
-    {
+const questions = [{
         question: 'what is the second name of kabul?',
-        answer: [
-            {text:"kabul",correct:true},
-            {text:"herat",correct:false},
-            {text:"ghazni",correct:false},
-            {text:"ghor",correct:false},
+        answer: [{
+                text: "kabul",
+                correct: true
+            },
+            {
+                text: "herat",
+                correct: false
+            },
+            {
+                text: "ghazni",
+                correct: false
+            },
+            {
+                text: "ghor",
+                correct: false
+            },
 
         ]
     },
     {
         question: 'what is css?',
-        answer: [
-            {text:"stands for Cascading Style Sheets",correct:true},
-            {text:"page",correct:false},
-            {text:"style ",correct:false},
-            {text:"design",correct:false},
+        answer: [{
+                text: "stands for Cascading Style Sheets",
+                correct: true
+            },
+            {
+                text: "page",
+                correct: false
+            },
+            {
+                text: "style ",
+                correct: false
+            },
+            {
+                text: "design",
+                correct: false
+            },
 
         ]
     },
     {
         question: 'what is html?',
-        answer: [
-            {text:"Hyper Text Markup Language",correct:true},
-            {text:"programming languge",correct:false},
-            {text:"coding",correct:false},
-            {text:"none",correct:false},
+        answer: [{
+                text: "Hyper Text Markup Language",
+                correct: true
+            },
+            {
+                text: "programming languge",
+                correct: false
+            },
+            {
+                text: "coding",
+                correct: false
+            },
+            {
+                text: "none",
+                correct: false
+            },
 
         ]
-    },{
+    }, {
         question: 'what is js?',
-        answer: [
-            {text:" scripting language",correct:true},
-            {text:"scripting",correct:false},
-            {text:"web",correct:false},
-            {text:"none",correct:false},
+        answer: [{
+                text: " scripting language",
+                correct: true
+            },
+            {
+                text: "scripting",
+                correct: false
+            },
+            {
+                text: "web",
+                correct: false
+            },
+            {
+                text: "none",
+                correct: false
+            },
 
         ]
     }
 ];
 
- const questionPart = document.getElementById("question");
- const answerButton = document.getElementById("answer");
- const nextButton = document.getElementById("next");
+const questionPart = document.getElementById("question");
+const answerButtons = document.getElementById("answer");
+const nextButton = document.getElementById("next");
 
 
- let currentQuestionIndex = 0;
- let score = 0;
+let currentQuestionIndex = 0;
+let score = 0;
 
- function startQuiz(){
+function startQuiz() {
     let currentQuestionIndex = 0;
     let score = 0;
     nextButton.innerHTML = "Next";
-     showQuestion();
+    showQuestion();
 
- }
- function showQuestion(){
+}
+
+function showQuestion() {
+    resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestion + 1;
     questionElement.innerHTML = questionNo + "." + currentQuestion.question;
- }
 
- currentQuestion.answers.forEach(answer => {
-    const button = document.createElement("button");
-    button.innerHTML = answer.text;
-    button.classList.add("btn");
-    answerButton.appendChild("button");
+
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+
+
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
     
-    
- });
+}
+
+
+startQuiz();
