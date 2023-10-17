ee509ef1ff7de024dcffcfc2cd0c23800ccb5d64
LimaZurmati <urzalalima@gmail.com>
2023-10-17 18:44:34 +0000

    add function to display answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,3 +60,12 @@ const questions = [
     let questionNo = currentQuestion + 1;
     questionElement.innerHTML = questionNo + "." + currentQuestion.question;
  }
+
+ currentQuestion.answers.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButton.appendChild("button");
+    
+    
+ });
