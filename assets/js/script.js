6f2847edd6a650ef2f56bb740b3303e04b8fa489
LimaZurmati <urzalalima@gmail.com>
2023-10-21 16:53:40 +0000

    add socre function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,60 @@
 const questions = [
     {
-        question: "what is the second name of kabul?",
+        question: "What is HTML??",
         answers: [
-            { text: "kabul", correct: true },
+            { text: "HTML stands for HyperText Markup Language", correct: true },
             {
-                text: "herat",
+                text: "text formatting language used for creating and displaying pages on the Internet",
                 correct: false
             },
             {
-                text: "ghazni",
-                correct: false
-            },
-            {
-                text: "ghor",
-                correct: false
-            },
-
-        ]
-    },
-    {
-        question: 'what is css?',
-        answers: [{
-                text: "stands for Cascading Style Sheets",
-                correct: true
-            },
-            {
-                text: "page",
-                correct: false
-            },
-            {
-                text: "style ",
-                correct: false
-            },
-            {
-                text: "design",
-                correct: false
-            },
-
-        ]
-    },
-    {
-        question: 'what is html?',
-        answers: [{
-                text: "Hyper Text Markup Language",
-                correct: true
-            },
-            {
-                text: "programming languge",
-                correct: false
-            },
-            {
-                text: "coding",
+                text: "HTML documents are made up of the elements and the tags that format it for proper display on pages.",
                 correct: false
             },
             {
                 text: "none",
+                correct: false
+            },
+
+        ]
+    },
+    {
+        question: 'How many sizes of headers are available in HTML by default?',
+        answers: [{
+                text: "6",
+                correct: true
+            },
+            {
+                text: "5",
+                correct: false
+            },
+            {
+                text: "3 ",
+                correct: false
+            },
+            {
+                text: "1",
+                correct: false
+            },
+
+        ]
+    },
+    {
+        question: 'What is the smallest header in HTML by default?',
+        answers: [{
+                text: "h6",
+                correct: true
+            },
+            {
+                text: "h5",
+                correct: false
+            },
+            {
+                text: "h4",
+                correct: false
+            },
+            {
+                text: "h1",
                 correct: false
             },
 
@@ -128,8 +128,47 @@ function showQuestion() {
     }
   }
   function selectAnswer (e){
+    const selectBtn = e.target;
+    const isCorrect = selectBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectBtn.classList.add("correct");
+        score++;
+
+
+    }else{
+        selectBtn.classList.add("incorrect")
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
 
   }
+  function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}`; 
+    nextButton.innerHTML = "play again";
+    nextButton.style.display = "block";
+  }
+   function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
 
+    }else{
+        showScore();
+    }
+   }
+ nextButton.addEventListener("click", ()=>{
+if(currentQuestionIndex < questions.length){
+    handleNextButton();
+
+}else{
+    startQuiz();
+}
+ });
 startQuiz();
 
